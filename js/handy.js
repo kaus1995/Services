5d1832f2ba3df9c5b603269f14b52cb5b4063c80
Kaustubh Srivastava <Kaustubh Srivastava>
2017-03-16 09:32:03 +0530

    Initial Commit of full repository

--- js/handy.js
+++ js/handy.js
@@ -3,16 +3,20 @@ var modal = document.getElementById('popupContact');
 
 // Get the button that opens the modal
 var btn = document.getElementById("btnAdd");
+var editBtn = document.getElementByID("edit");
 
 // Get the <span> element that closes the modal
 var span = document.getElementsByClassName("close")[0];
+
+//Get the button that adds the card
+var submit = document.getElementById("submit");
 
 // When the user clicks on the button, open the modal 
 btn.onclick = function() {
     modal.style.display = "block";
 }
 
-// When the user clicks on <span> (x), close the modal
+// When the user clicks on (x), close the modal
 span.onclick = function() {
     modal.style.display = "none";
 }
@@ -22,4 +26,14 @@ window.onclick = function(event) {
     if (event.target == modal) {
         modal.style.display = "none";
     }
+}
+
+//when the user clicks the edit button on the card
+editBtn.onclick = function(){
+    modal.style.display = "block";
+}
+
+//adding user info card when submit button is clicked
+submit.onclick = function(){
+    document.getElementById("userCard").style.display = "block";
 }
